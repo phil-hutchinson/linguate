d6a0cdb9f5ba3ad857840d2cd377e5c6b48081f5
phil-hutchinson <phil-hutchinson@users.noreply.github.com>
2016-08-18 14:26:07 -0700

    License Clarification

--- src/org/linguate/compile/ParseNodeFactory.java
+++ src/org/linguate/compile/ParseNodeFactory.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This code has not bee released under any license.
+ * In future, it will be released under an open-source license.
  */
 package org.linguate.compile;
 
