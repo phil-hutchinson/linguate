be66c858fdc19040acca9355b2666d4da9990293
phil-hutchinson <phil-hutchinson@users.noreply.github.com>
2016-11-02 14:43:50 -0700

    Documentation for lexeme pacakage. Advances #2.

--- src/org/linguate/compile/lexeme/Lexeme.java
+++ src/org/linguate/compile/lexeme/Lexeme.java
@@ -7,11 +7,25 @@ package org.linguate.compile.lexeme;
 import org.linguate.compile.grammar.GrammarTerminal;
 
 /**
- *
+ * A lexeme is an individual "word". It is the output of a lexer, which turns a 
+ * stream of characters into a stream of lexemes, and the input of a parser,
+ * which turns a stream of lexemes into a parse tree.
+ * 
  * @author Phil Hutchinson
  */
 public interface Lexeme
 {
+
+    /**
+     * Returns the grammar terminal associated with this lexeme.
+     * @return The grammar terminal this lexeme represents
+     */
     GrammarTerminal getElement();
+
+    /**
+     * Returns the contents of the lexeme. This will usually be the
+     * set of characters in the input stream that were used to create it.
+     * @return The contents of the lexeme
+     */
     String getContents();
 }
